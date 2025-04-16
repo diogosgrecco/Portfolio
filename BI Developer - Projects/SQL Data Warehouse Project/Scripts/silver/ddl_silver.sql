5730d0ba0901b13efa3f77eb875847fb9caa5d3d
camus100 <diogosgrecco@gmail.com>
2025-04-16 11:23:29 +1000

    Update ddl_silver.sql

--- BI Developer - Projects/SQL Data Warehouse Project/Scripts/silver/ddl_silver.sql
+++ BI Developer - Projects/SQL Data Warehouse Project/Scripts/silver/ddl_silver.sql
@@ -24,39 +24,6 @@ CREATE TABLE silver.crm_cust_info (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
 
-INSERT INTO silver.crm_cust_info (
-	cst_id, 
-	cst_key, 
-	cst_firstname, 
-	cst_lastname, 
-	cst_material_status, 
-	cst_gndr, 
-	cst_create_date
-)
-SELECT 
-	cst_id,
-	cst_key,
-	TRIM(cst_firstname) cst_firstname,
-	TRIM(cst_lastname) cst_lastname,
-	CASE UPPER(TRIM(cst_material_status)) 
-		WHEN 'S' THEN 'Single'
-		WHEN 'M' THEN 'Married'
-		ELSE 'n/a'
-		END cst_material_status,
-	CASE WHEN UPPER(TRIM(cst_gndr)) = 'F' THEN 'Female'
-		WHEN UPPER(TRIM(cst_gndr)) = 'M' THEN 'Male'
-		ELSE 'n/a'
-		END cst_gndr,
-	cst_create_date
-FROM (
-	SELECT 
-	*,
-	ROW_NUMBER() OVER (PARTITION BY cst_id ORDER BY cst_create_date DESC) 'flag_last'
-	FROM bronze.crm_cust_info
-	WHERE cst_id IS NOT NULL
-	) t
-WHERE t.flag_last = 1
-	
 IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
 	DROP TABLE silver.crm_prd_info
 GO
@@ -72,33 +39,6 @@ CREATE TABLE silver.crm_prd_info (
 	prd_end_dt DATE,
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
-
-INSERT INTO silver.crm_prd_info (
-	prd_id,
-	cat_id,
-	prd_key,
-	prd_nm,
-	prd_cost,
-	prd_line,
-	prd_start_dt,
-	prd_end_dt
-)
-SELECT 
-	prd_id,
-	REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') cat_id,
-	REPLACE(SUBSTRING(prd_key, 7, LEN(prd_key)),'-','_') prd_key,
-	prd_nm,
-	ISNULL(prd_cost,0) prd_cost,
-	CASE UPPER(TRIM(prd_line)) 
-		WHEN 'M' THEN 'Mountain'
-		WHEN 'R' THEN 'Road'
-		WHEN 'S' THEN 'Other Sales'
-		WHEN 'T' THEN 'Touring'
-		ELSE 'n/a'
-		END prd_line,
-	prd_start_dt,
-	prd_end_dt
-FROM bronze.crm_prd_infoselect 1/null
 
 IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
 	DROP TABLE silver.crm_sales_details
@@ -117,41 +57,6 @@ CREATE TABLE silver.crm_sales_details (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
 
-INSERT INTO silver.crm_sales_details (
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	sls_order_dt,
-	sls_ship_dt,
-	sls_due_dt,
-	sls_sales,
-	sls_quantity,
-	sls_price
-)
-SELECT
-	sls_ord_num,
-	sls_prd_key,
-	sls_cust_id,
-	CASE WHEN sls_order_dt = 0 OR LEN(sls_order_dt) != 8 THEN NULL
-	ELSE CAST(CAST(sls_order_dt AS varchar) AS DATE)
-	END sls_order_dt,
-	CASE WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) != 8 THEN NULL
-	ELSE CAST(CAST(sls_ship_dt AS varchar) AS DATE)
-	END sls_ship_dt,
-	CASE WHEN sls_due_dt = 0 OR LEN(sls_due_dt) != 8 THEN NULL
-	ELSE CAST(CAST(sls_due_dt AS varchar) AS DATE)
-	END sls_due_dt,
-	CASE WHEN sls_sales IS NULL OR sls_sales <=0 OR sls_sales != sls_quantity * ABS(sls_price)
-		THEN sls_quantity * ABS(sls_price)
-		ELSE sls_sales
-		END sls_sales,
-	sls_quantity,
-	CASE WHEN sls_price IS NULL OR sls_price <=0
-		THEN sls_sales / sls_quantity
-		ELSE sls_price
-		END sls_price
-FROM bronze.crm_sales_details
-	
 IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
 	DROP TABLE silver.erp_cust_az12
 GO
@@ -163,24 +68,6 @@ CREATE TABLE silver.erp_cust_az12 (
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
 
-INSERT INTO silver.erp_cust_az12 (
-	cid,
-	bdate,
-	gen
-)
-SELECT
-	CASE WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid,4,LEN(cid))
-	ELSE cid
-	END cid,
-	CASE WHEN bdate > GETDATE() THEN NULL
-	ELSE bdate
-	END bdate,
-	CASE WHEN UPPER(TRIM(gen)) IN ('F', 'FEMALE') THEN 'Female'
-		WHEN UPPER(TRIM(gen)) IN ('M', 'MALE') THEN 'Male'
-		ELSE 'n/a'
-	END gen
-FROM bronze.erp_cust_az12
-
 IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE silver.erp_loc_a101
 GO
@@ -190,19 +77,6 @@ CREATE TABLE silver.erp_loc_a101 (
 	cntry VARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
-
-INSERT INTO silver.erp_loc_a101 (
-	cid,
-	cntry
-)
-SELECT 
-	REPLACE(cid,'-','') cid,
-	CASE WHEN TRIM(cntry) = 'DE' THEN 'Germany'
-		WHEN TRIM(cntry) IN ('US', 'USA') THEN 'United States'
-		WHEN TRIM(cntry) ='' THEN 'n/a'
-		ELSE TRIM(cntry)
-		END cntry
-FROM bronze.erp_loc_a101
 
 IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
 	DROP TABLE silver.erp_px_cat_g1v2
@@ -215,13 +89,3 @@ CREATE TABLE silver.erp_px_cat_g1v2 (
 	maintenance VARCHAR(50),
 	dwh_create_date DATETIME2 DEFAULT GETDATE()
 )
-
-INSERT INTO silver.erp_px_cat_g1v2 (
-	id,
-	cat,
-	subcat,
-	maintenance
-)
-SELECT
-	*
-FROM bronze.erp_px_cat_g1v2
