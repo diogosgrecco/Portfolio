e3a56fd0a5a543685c58e81188f3199351574289
camus100 <diogosgrecco@gmail.com>
2025-04-16 16:32:23 +1000

    Update proc_load_silver.sql

--- BI Developer - Projects/SQL Data Warehouse Project/Scripts/silver/proc_load_silver.sql
+++ BI Developer - Projects/SQL Data Warehouse Project/Scripts/silver/proc_load_silver.sql
@@ -87,9 +87,10 @@ BEGIN
 			WHEN 'T' THEN 'Touring'
 			ELSE 'n/a'
 			END prd_line,
-		prd_start_dt,
-		prd_end_dt
+		CAST(prd_start_dt AS DATE) prd_start_dt,
+		CAST(DATEADD(D,-1,LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)) AS DATE) prd_end_dt
 	FROM bronze.crm_prd_info
+	where prd_key='AC-HE-HL-U509-R'
 	
 	SET @end_time=GETDATE()
 	PRINT '>> Load Duration: ' + CAST(DATEDIFF(second,@start_time,@end_time) AS VARCHAR) + ' seconds'
